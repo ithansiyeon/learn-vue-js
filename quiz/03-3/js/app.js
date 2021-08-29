be2626cc58e10b2581eb9fdfcd914c1895a7b149
ithansiyeon <itsiyeon@nate.com>
2021-08-29 16:02:37 +0900

    commit

---
+++ quiz/03-3/js/app.js
@@ -0,0 +1,21 @@
+Vue.component('child-component',{
+    props:['propsdata'],
+    template:'<p>{{propsdata}}</p>'
+});
+
+Vue.component('sibling-component',{
+    props:['propsdata'],
+    template:'<p>{{propsdata}}</p>'
+});
+
+var app = new Vue({
+    el:'#app',
+    data:{
+        message:'Hello Vue! passed from Parent Component',
+        anotherMessage:'Hello Vue! passed from Parent Component',
+    }
+});
+
+
+
+
