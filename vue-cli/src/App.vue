826933cdd643a7c675bec35fd6eb9a2703d1ab1f
ithansiyeon <itsiyeon@nate.com>
2021-10-10 18:41:17 +0900

    commit

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,13 +1,30 @@
 <template>
-  <div>
-    App
-  </div>
+  <!-- <app-header v-bind:프롭스 속성 이름="상위 컴포넌트의 데이터 이름"></app-header> -->
+  <app-header v-bind:propsdata="str"
+  v-on:renew="renewStr"></app-header>
 </template>
 
 <script>
+import AppHeader from './components/AppHeader.vue';
+// var AppHeader = {
+//   template:'<header><h1>Header</h1></header>'
+// }
 export default {
-
+  data: function (){
+    return {
+      str:'Header'
+    }
+  },
+  components: {
+    'app-header':AppHeader
+  },
+  methods: {
+    renewStr:function(){
+      this.str = 'hi';
+    }
+  }
 }
+
 </script>
 
 <style>
